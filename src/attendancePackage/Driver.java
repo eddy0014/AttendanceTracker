31d7c9031424c47b1b2c0dbdf5506786f1978211
Eddy Salazar <e-salazar@outlook.com>
2017-03-31 01:17:36 -0500

    Added database code

---
+++ src/attendancePackage/Driver.java
@@ -0,0 +1,123 @@
+package attendancePackage;
+
+import java.sql.*;
+
+public class Driver {
+	
+	Connection dbConnection;
+
+	public void createConnection() {
+		try{
+			// Get a connection to database
+			dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/attendance_tracker", "root", "data-manage55");
+			
+			// Create a statement
+			//Statement myStatement = dbConnection.createStatement();
+			
+			// Execute SQL query
+			//ResultSet myResult = myStatement.executeQuery("select * from city limit 0, 10");
+			
+			// Process result set
+			/*while(myResult.next()) {
+				System.out.println(myResult.getString("city") + ", " + myResult.getString("city_id")); 
+			}*/
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	//TODO add code to create attendance table for the course
+	public void addCourse(String courseNum, String courseName) {
+		try {
+			// Create a statement
+			Statement theStatement = dbConnection.createStatement();
+			
+			// Insert course information into courses table
+			String insertCourse = "insert into courses (course_num, course_name) values "
+					+ "(" + "\"" + courseNum + "\"" + "," + "\"" + courseName + "\"" + ")";
+			
+			String courseNumLower = courseNum.toLowerCase();
+			
+			// Create the table for the course
+			String createTable = "create table " + courseNumLower + " (student_id int, first_name tinytext, last_name tinytext)";
+			
+			// Create the attendance table for the course
+			String createAttendTable = "create table attendance_" + courseNumLower + " (student_id int, first_name tinytext, last_name tinytext, status tinytext)";
+			
+			// Execute queries
+			int rowsAffected = theStatement.executeUpdate(insertCourse); 
+			theStatement.executeUpdate(createTable);
+			theStatement.executeUpdate(createAttendTable);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ResultSet selectStudentsInCourse(String courseNum) {
+		ResultSet results = null;
+		
+		try{
+			Statement theStatement = dbConnection.createStatement();
+			
+			String courseNumLower = courseNum.toLowerCase();
+			String query = "select * from " + courseNumLower + " order by last_name";
+			
+			results = theStatement.executeQuery(query);
+		} catch(Exception e) {
+			e.printStackTrace(); 
+		}
+		
+		return results;
+	}
+	
+	public int countStudentsInCourse(String courseNum) {
+		int numOfRows = 0;
+		
+		try {
+			Statement theStatement = dbConnection.createStatement();
+			
+			String courseNumLower = courseNum.toLowerCase();
+			String query = "select count(*) as total from " + courseNumLower;
+			ResultSet results = theStatement.executeQuery(query);
+			while(results.next()) {
+				numOfRows = results.getInt("total"); 
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return numOfRows;
+	}
+	
+	public void addStudent(String courseNum, int id, String firstName, String lastName) {
+		try {
+			Statement theStatement = dbConnection.createStatement();
+			
+			String courseNumLower = courseNum.toLowerCase();
+			String query = "insert into " + courseNumLower + "(student_id, first_name, last_name) values "
+					+ "(" + id + "," + "\"" + firstName + "\"" + "," + "\"" +  lastName + "\"" + ")";
+			
+			int rowsAffected = theStatement.executeUpdate(query); 
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void insertIntoAttend(String courseNum, int id, String firstName, String lastName, String statusLabel) {
+		try {
+			Statement theStatement = dbConnection.createStatement();
+			
+			String courseNumLower = courseNum.toLowerCase();
+			String query = "insert into attendance_" + courseNumLower + " (student_id, first_name, last_name, status) values "
+					+ "(" + id + "," + "\"" + firstName + "\"" + "," + "\"" +  lastName + "\"" + "," + "\"" + statusLabel + "\"" + ")";
+			
+			int rowsAffected = theStatement.executeUpdate(query); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
--- src/attendancePackage/Main.java
+++ src/attendancePackage/Main.java
@@ -3,16 +3,23 @@ package attendancePackage;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
+import java.sql.*;
 
 public class Main extends JFrame {
 	
 	private JPanel parentPanel;
 	private JMenuBar appMenuBar;
 	private JMenu menu;
+	Driver db;
 	
 	public Main() {
+		// Connect to the database
+		db = new Driver();
+		db.createConnection();
+	}
+	
+	public void setupEverything() {
 		setLayout(null);
 		
 		setTitle("Attendance Tracking");
@@ -58,23 +65,37 @@ public class Main extends JFrame {
 
 		// This can be used to make the menu look cleaner
 		//menu.addSeparator();
+		
+		setVisible(true);
 	}
 	
 	public void addCourse() {
-		// Set layout for panel
+		// Set layout for panel asking for course information
 		JPanel addCoursePanel = new JPanel();
-		addCoursePanel.add(new JLabel("Enter course number:"));
+		addCoursePanel.setLayout(new BoxLayout(addCoursePanel, BoxLayout.PAGE_AXIS)); 
+		JPanel courseNumPanel = new JPanel();
+		JPanel courseNamePanel = new JPanel();
+		courseNumPanel.add(new JLabel("Enter course number:"));
 		JTextField courseNumField = new JTextField(7);
-		addCoursePanel.add(courseNumField); 
+		courseNumPanel.add(courseNumField);
+		courseNamePanel.add(new JLabel("Enter course name:"));
+		JTextField courseNameField = new JTextField(12);
+		courseNamePanel.add(courseNameField); 
+		addCoursePanel.add(courseNumPanel);
+		addCoursePanel.add(courseNamePanel);
 		
-		// Show panel and save option the user selects
+		// Show panel and save the option the user selects
 		int userButtonChoice = JOptionPane.showConfirmDialog(null, addCoursePanel, 
 				"Add Course", JOptionPane.OK_CANCEL_OPTION);
 		
 		String courseNum = courseNumField.getText();
+		//String courseNum = "CSC_150";
+		String courseName = courseNameField.getText();
 		
 		// Check to see what the user selected
 		if(userButtonChoice == JOptionPane.OK_OPTION) {
+			// Add the course to the database
+			db.addCourse(courseNum, courseName);
 			displayCourseHomePanel(courseNum);
 		} else {
 			// Do nothing
@@ -89,26 +110,174 @@ public class Main extends JFrame {
 		parentCoursePanel.setLayout(new BoxLayout(parentCoursePanel, BoxLayout.LINE_AXIS));
 		parentCoursePanel.setSize(500, 500);
 				
+		// The panel displaying the buttons for options
 		JPanel leftCoursePanel = new JPanel();
 		leftCoursePanel.setPreferredSize(new Dimension(25, 500));
-		leftCoursePanel.setBackground(Color.blue);
+		leftCoursePanel.setBackground(Color.orange);
 		leftCoursePanel.add(new JLabel(courseNum));
-		parentCoursePanel.add(leftCoursePanel);
 		
+		// Set the buttons
+		JButton takeButton = new JButton("Take Attendance");
+		leftCoursePanel.add(takeButton);
+		JButton manageButton = new JButton("Manage Students");
+		leftCoursePanel.add(manageButton);
+		
+		parentCoursePanel.add(leftCoursePanel);
+	
+		// The panel displaying the content
 		JPanel rightCoursePanel = new JPanel();
 		rightCoursePanel.setPreferredSize(new Dimension(250, 500));
 		rightCoursePanel.setBackground(Color.red);
 		parentCoursePanel.add(rightCoursePanel);
+		
+		manageButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JPanel managePanel = new JPanel();
+				managePanel.setBackground(Color.cyan);
+				managePanel.setPreferredSize(new Dimension(rightCoursePanel.getWidth(), rightCoursePanel.getHeight())); 
+				
+				ResultSet courseStudents = db.selectStudentsInCourse(courseNum); 
+				
+				try { 
+				while(courseStudents.next()) {
+					// TODO fix this so it matches database table in columns
+					managePanel.add(new JLabel(courseStudents.getString("first_name") + " " + courseStudents.getString("last_name")));
+				}
+				} catch (Exception e) {
+					e.printStackTrace(); 
+				}
+				
+				JButton addStudentButton = new JButton("Add student");
+				managePanel.add(addStudentButton);
+				
+				addStudentButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						addStudent(courseNum);
+					}
+				});
+				
+				rightCoursePanel.add(managePanel);
+				rightCoursePanel.revalidate();
+				rightCoursePanel.repaint();
+			}
+		});
+		
+		takeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				displayTakeAttPanel(courseNum, rightCoursePanel);
+			}
+		});
 		
 		// Add the new course to the menu
 		menu.add(new JMenuItem(courseNum));
 		
 		add(parentCoursePanel);
 	}
+	
+	public void displayManagePanel(String courseNum) {
+		
+	}
+	
+	public void displayTakeAttPanel(String courseNum, JPanel rightCoursePanel) {
+		// Set up panel layout
+		JPanel takePanel = new JPanel();
+		takePanel.setLayout(new BoxLayout(takePanel, BoxLayout.PAGE_AXIS));
+		takePanel.setBackground(Color.cyan);
+		takePanel.setPreferredSize(new Dimension(rightCoursePanel.getWidth(), rightCoursePanel.getHeight())); 
+		
+		int numOfStudents = db.countStudentsInCourse(courseNum);
+		
+		JComboBox[] comboArray = new JComboBox[numOfStudents];
+		String[] labels = {"Present", "Absent", "Excused"};
+		int i = 0;
+		
+		ResultSet studentsInCourse = db.selectStudentsInCourse(courseNum);
+		
+		try {
+			while(studentsInCourse.next()) {
+				JPanel pane = new JPanel();
+				
+				JLabel name = new JLabel(studentsInCourse.getString("first_name") + " " + studentsInCourse.getString("last_name"));
+				
+				comboArray[i] = new JComboBox(labels);
+				
+				pane.add(name);
+				pane.add(comboArray[i]);
+				takePanel.add(pane);
+				
+				i++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		JButton saveButton = new JButton("Save");
+		takePanel.add(saveButton); 
+		
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				int j = 0;
+				
+				ResultSet studentsInCourse2 = db.selectStudentsInCourse(courseNum);
+				
+				try {
+					while(studentsInCourse2.next()) {												
+						int studentID = studentsInCourse2.getInt("student_id"); 
+						String firstName = studentsInCourse2.getString("first_name");
+						String lastName = studentsInCourse2.getString("last_name");
+						String statusLabel = (String) comboArray[j].getSelectedItem();
+						
+						db.insertIntoAttend(courseNum, studentID, firstName, lastName, statusLabel);  
+						
+						j++;
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		rightCoursePanel.add(takePanel);
+		rightCoursePanel.revalidate();
+		rightCoursePanel.repaint();
+	}
+	
+	public void addStudent(String courseNum) {
+		JPanel addStudentPanel = new JPanel();
+		addStudentPanel.setLayout(new BoxLayout(addStudentPanel, BoxLayout.PAGE_AXIS)); 
+		JPanel idPanel = new JPanel();
+		JPanel firstnamePanel = new JPanel();
+		JPanel lastNamePanel = new JPanel();
+		idPanel.add(new JLabel("Student ID:"));
+		JTextField idField = new JTextField(10);
+		idPanel.add(idField);
+		firstnamePanel.add(new JLabel("First Name:"));
+		JTextField firstNameField = new JTextField(7);
+		firstnamePanel.add(firstNameField);
+		lastNamePanel.add(new JLabel("Last Name:"));
+		JTextField lastNameField = new JTextField(12);
+		lastNamePanel.add(lastNameField); 
+		addStudentPanel.add(idPanel);
+		addStudentPanel.add(firstnamePanel);
+		addStudentPanel.add(lastNamePanel);
+		
+		int userButtonChoice = JOptionPane.showConfirmDialog(null, addStudentPanel, 
+				"Add Student", JOptionPane.OK_CANCEL_OPTION);
+		
+		if(userButtonChoice == JOptionPane.OK_OPTION) {
+			int id = Integer.parseInt(idField.getText());
+			String firstName = firstNameField.getText();
+			String lastName = lastNameField.getText();
+			
+			db.addStudent(courseNum, id, firstName, lastName); 
+		} else {
+			// Do nothing
+		}
+	}
 
-	public static void main(String[] args) {
-		Main myApp = new Main();
-		myApp.setVisible(true);
+	public static void main(String[] args) {		
+		Main theApp = new Main();
+		theApp.setupEverything();
 	}
 
 }
